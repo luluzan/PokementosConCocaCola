5645a2d38debd55ee277313d386a5b66d748f57d
César Conte Pérez <128363248+cesarconte@users.noreply.github.com>
2023-06-23 12:53:45 +0200

    PokemonLittlecard.vue init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
+import PokemonLittleCard from '../components/PokemonLittleCard.vue';
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <PokemonLittleCard />
   </main>
 </template>
