99bd209dd7d734523e5539f76573d4cf9994aa21
César Conte Pérez <128363248+cesarconte@users.noreply.github.com>
2023-06-23 15:38:46 +0200

    remove unnecessary stuff

--- src/components/PokemonLittleCard.vue
+++ src/components/PokemonLittleCard.vue
@@ -1,21 +1,44 @@
 <template>
-  <v-card class="mx-auto" max-width="400">
-    <v-img class="align-end text-white" height="200" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover>
+  <v-card class="mx-auto" max-width="300">
+    <v-img class="align-end text-white" height="300" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover>
       <v-card-title>ID: </v-card-title>
     </v-img>
 
-    <v-card-subtitle class="pt-4">
-      Name:
-    </v-card-subtitle>
+    <v-card-subtitle class="pt-4"> Name: </v-card-subtitle>
 
     <v-card-text>
-      <div>Type: </div>
+      <div>Type:</div>
     </v-card-text>
 
     <v-card-actions>
-      <v-btn color="orange">
-        Share
-      </v-btn>
+      <div class="text-center">
+        <v-btn color="error" @click="overlay = !overlay"> Click me! </v-btn>
+
+        <v-overlay v-model="overlay" class="align-center justify-center">
+          <v-card class="mx-auto" max-width="600">
+            <v-img class="align-end text-white" height="600" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover>
+              <v-card-title> ID: </v-card-title>
+            </v-img>
+
+            <v-card-subtitle class="pt-4"> Name: </v-card-subtitle>
+
+            <v-card-text>
+              <div>Type:</div>
+              <div>Other:</div>
+              <div>Height:</div>
+              <div>Weight:</div>
+            </v-card-text>
+          </v-card>
+        </v-overlay>
+      </div>
     </v-card-actions>
   </v-card>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    overlay: false,
+  }),
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup>
 import PokemonLittleCard from '../components/PokemonLittleCard.vue';
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
     <PokemonLittleCard />
   </main>
 </template>
