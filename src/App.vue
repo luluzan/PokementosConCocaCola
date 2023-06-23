6192686b3702bf2499fb9053af18dc83b56e84db
LuLu <lu.luzan@gmail.com>
2023-06-23 14:08:31 +0200

    bars

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <header>
+    <v-toolbar density="comfortable" color="#C83D34"></v-toolbar>
+    <v-divider   :thickness="5"   class="border-opacity-100"  color="warning"></v-divider>
+    <v-toolbar density="comfortable" color="#1D2C5E"></v-toolbar>
+    <v-img  :width="300"  aspect-ratio="16/9"   variant=elevado src="../public/imgs/Lupa_29.webp"></v-img>
+  </header>
 
   <h1>Hola Frikis</h1>
 
