522cab83d03e7da4b7fef0bec1259de8467b08f5
LuLu <lu.luzan@gmail.com>
2023-06-26 11:11:04 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -14,33 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
   
 
-  <!-- <h1>Hola Frikis</h1>
 
-  <div class="text--primary">
-    <!-- Using the elevation prop -->
-    <v-hover v-slot="{ isHovering, props }">
-      <v-card
-        v-bind="props"
-        :elevation="isHovering ? 24 : 6"
-        class="mx-auto pa-6"
-      >
-        Prop based elevation
-      </v-card>
-    </v-hover>
-
-    <div class="my-6"></div>
-
-    <!-- Using a dynamic class -->
-    <v-hover v-slot="{ isHovering, props }">
-      <div
-        v-bind="props"
-        :class="`elevation-${isHovering ? 24 : 6}`"
-        class="mx-auto pa-6 transition-swing"
-      >
-        Class based elevation
-      </div>
-    </v-hover>
-  </div> -->
 
 </main>
 </template>
