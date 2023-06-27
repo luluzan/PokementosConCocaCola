f7b0baee97b881a78557325e129dda3de8f34ae9
LuLu <lu.luzan@gmail.com>
2023-06-27 13:53:17 +0200

    logo

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,14 @@ import FooterComponent from './components/FooterComponent.vue'
 
 <template>
 
-  <header>
+<header>
+    <div id="vacio">
+      <img src="../public/imgs/logo.png" alt/>
+    </div>
     <v-toolbar density="comfortable" color="#C83D34"></v-toolbar>
     <v-divider   :thickness="5"   class="border-opacity-100"  color="warning"></v-divider>
     <v-toolbar density="comfortable" color="#1D2C5E"></v-toolbar>
-    <v-img  :width="300"  aspect-ratio="16/9"   variant=elevation src="../public/imgs/logo.png"></v-img>
+   
   </header>
 <main>
   <ToolBarsSearch/>
