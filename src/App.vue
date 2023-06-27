ad46ddb9f524e3d45e5f1fa79c004f0c943333ce
David <kerotupy@hotmail.com>
2023-06-27 11:10:26 +0200

    actualizo dev en mi local

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import GridContainer from './components/GridContainer.vue';
 </script>
 
 <template>
 
   <h1>Hola Frikis</h1>
 
-
+ <GridContainer />
 </template>
 
 <style scoped>
-
 
 </style>
---
+++ src/components/GridContainer.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <v-container class="bg-surface-variant mb-6">
+            <v-row  no-gutters medida>
+
+                <v-col cols="12" sm="6" lg="3" elevation="24" class="medida">
+                   <h2>contenido</h2>                   
+                </v-col>
+
+                <v-col cols="12" sm="6" lg="3" elevation="24" class="medida">
+                   <h2>contenido</h2>                   
+                </v-col>
+
+                <v-col cols="12" sm="6" lg="3" elevation="24" class="medida">
+                   <h2>contenido</h2>                   
+                </v-col>
+
+                <v-col cols="12" sm="6" lg="3" elevation="24" class="medida">
+                   <h2>contenido</h2>                   
+                </v-col>
+
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+ export default {
+    name: 'GridContainer'
+ }
+</script>
+
+<style>
+.medida {
+    text-align: center;
+    width: 300px;
+    height: 200px;
+    background-color: aqua;
+    border: 3px solid black;
+}
+
+</style>
