de95eab41e0455b054bbc01706b4a15ef902d8e6
LuLu <lu.luzan@gmail.com>
2023-06-27 11:32:36 +0200

    Main con toolbar y card

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-<<<<<<< HEAD
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import ToolBarsSearch from './components/ToolBarsSearch.vue'
@@ -12,29 +11,15 @@ import ToolBarsSearch from './components/ToolBarsSearch.vue'
     <v-toolbar density="comfortable" color="#1D2C5E"></v-toolbar>
     <v-img  :width="300"  aspect-ratio="16/9"   variant=elevation src="../public/imgs/logo.png"></v-img>
   </header>
- <main> 
-<HelloWorld/>
- <ToolBarsSearch/> 
+<main> 
+  <ToolBarsSearch/> 
 
 
 
-</main>
-=======
-//import { RouterLink, RouterView } from 'vue-router'
-//import HomeView from './components/HomeView.vue'
-import PokemonLittleCard from './components/PokemonLittleCard.vue';
-</script>
-
-<template>
   <PokemonLittleCard />
-  <!--<RouterView /> -->
->>>>>>> 587635f0db0feafa6c2c753d1e72974992bf7933
+</main>
 </template>
 
 <style scoped>
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 587635f0db0feafa6c2c753d1e72974992bf7933
 </style>
